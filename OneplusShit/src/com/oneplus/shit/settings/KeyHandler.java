0afba5bbf29fa6bc4d0a42c4c160ab4336594e21
ritiksaini5464 <67448300+ritiksaini5464@users.noreply.github.com>
2020-11-18 20:08:02 +0530

    Update KeyHandler.java

--- OneplusShit/src/com/oneplus/shit/settings/KeyHandler.java
+++ OneplusShit/src/com/oneplus/shit/settings/KeyHandler.java
@@ -43,7 +43,6 @@ import android.view.KeyEvent;
 import com.android.internal.os.DeviceKeyHandler;
 import com.android.internal.util.ArrayUtils;
 
-import lineageos.providers.LineageSettings;
 
 public class KeyHandler implements DeviceKeyHandler {
 
